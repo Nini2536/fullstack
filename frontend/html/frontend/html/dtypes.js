dce33a531eeb0716408b1f2f3f10be246674ead3
Nini2536 <nishasoundar64@gmail.com>
2026-01-09 16:04:16 +0530

    Add files via upload

---
+++ frontend/html/frontend/html/dtypes.js
@@ -0,0 +1,23 @@
+/*var x=10;
+var x=110;
+console.log("X:",x);
+let y=20;
+let y=120;
+console.log("Y:",y);
+const z=7;
+const z=17;
+console.log("Z:",z);*/
+
+/*var x=20;
+x=67;
+let y=65;
+y=87;
+const c=89;
+c=34;*/
+
+x=14;
+var x;
+y=90;
+let y;
+c=86;
+const c;
---
+++ frontend/html/frontend/html/map.js
@@ -0,0 +1,7 @@
+console.log("WELCOME TO WEBSITE");
+var a=80;
+var v=7;
+console.log("add:",a+v);
+console.log("sub:",a-v);
+console.log("mul:",a*v);
+console.log("div:",a/v);
