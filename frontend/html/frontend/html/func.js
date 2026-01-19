ddf220cd69262893c81caecf0dce0423d710d2d9
Nini2536 <nishasoundar64@gmail.com>
2026-01-19 16:42:14 +0530

    Add files via upload

---
+++ frontend/html/frontend/html/func.js
@@ -0,0 +1,9 @@
+var m=0;
+function skills()
+{
+    function add(){
+        console.log("YOUR CURRENT SCORE:",m,".Need to improve in programming.");
+    }
+    return;
+}
+skills.add;
---
+++ frontend/html/frontend/html/obj.js
@@ -0,0 +1,19 @@
+/*var shop={sweet:"laddu",type:"orange"};
+console.log(shop);
+console.log(shop.sweet);
+let choco=["dairymilk","munch","kitkat","koppiko"];
+console.log(choco[2]);*/
+
+
+let arr=[2,3,4,5,6];
+console.log(arr[3]-arr[1])
+
+
+var avg=[78,87,97,67,75];
+var sum=0;
+for(var i=0;i<avg.length;i++){
+    sum+=avg[i];
+}
+var mark=sum/avg.length;
+console.log(mark);
+
